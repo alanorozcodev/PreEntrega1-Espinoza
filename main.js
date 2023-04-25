d2e4db10ecf14872a0105165e1e3f91203e57db4
Alan Espinoza <alan.orozco43@gmail.com>
2023-04-24 21:11:45 -0600

    Se reliza la primera pre entrega del curso de JS

---
+++ main.js
@@ -0,0 +1,64 @@
+let costoTotal = 0;
+let pagoConTarjeta = 0;
+const limpieza = 150;
+const extraccion = 300;
+const endodoncia = 500;
+const blanqueamiento = 120;
+const sumaConTarjeta = (a, b) => a + b;
+const comision = x => x * .04;
+let costoServicio = 0;
+
+alert(`Que tal bienvenido a tu clínica dental , a continuación mostramos los servicios dentales que tenemos a tu disposición.
+
+Los servicios disponibles son:
+- Limpieza
+- Extracción
+- Endodoncia
+- Blanqueamiento`);
+
+while (true) {
+    let servicioSeleccionado = prompt("Ingresa el servicio que necesitas sin acentos (o escribe 'listo' para finalizar):").toLowerCase();
+
+    if (servicioSeleccionado === "listo") {
+        break;
+    }
+    if (servicioSeleccionado === "limpieza") {
+        costoServicio = limpieza;
+        costoTotal = costoTotal + costoServicio;
+    } else if (servicioSeleccionado === "extraccion") {
+        costoServicio = extraccion;
+        costoTotal = costoTotal + costoServicio;
+    } else if (servicioSeleccionado === "endodoncia") {
+        costoServicio = endodoncia;
+        costoTotal = costoTotal + costoServicio;
+    } else if (servicioSeleccionado === "blanqueamiento") {
+        costoServicio = blanqueamiento;
+        costoTotal = costoTotal + costoServicio;
+    } else {
+        alert("Lo siento, el servicio ingresado no está disponible.");
+        continue;
+    }
+    alert(`Servicio agregado: ${servicioSeleccionado} Costo: $${costoServicio}`);
+}
+alert(`El costo total de los servicios seleccionados es: $${costoTotal}`);
+
+
+
+while (true) {
+    pagoConTarjeta = prompt("¿Piensa pagar con efectivo o tarjeta? (El uso de tarjeta tiene una comisión del 4%)").toLowerCase();
+    if (pagoConTarjeta === "efectivo" || pagoConTarjeta === "tarjeta") {
+        break;
+    } else {
+        alert("Favor de ingresar si se pagará con efectivo o tarjeta.");
+    }
+}
+switch (pagoConTarjeta) {
+    case "tarjeta":
+        let costoTotalConTarjeta = sumaConTarjeta(costoTotal, comision(costoTotal));
+        alert(`El costo total de los servicios seleccionados más la comisión de pago con tarjeta es: $${costoTotalConTarjeta}`);
+        break;
+    case "efectivo":
+        alert(`El costo total de los servicios seleccionados es: $${costoTotal}`);
+        break;
+}
+alert("Que tengas un excelente día, esperamos tu visita.");
